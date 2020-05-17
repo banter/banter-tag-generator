0874104c82f750164d72bb29710e1184391caa61
Austin Marchese <amarchese3@gmail.com>
2020-05-17 02:22:55 -0400

    added basic base handler for non sports

--- src/main/tagging_algos/tagging_base_handler.py
+++ src/main/tagging_algos/tagging_base_handler.py
@@ -22,19 +22,14 @@ class TaggingBaseHandler:
 
         return person_tags
 
-
     def get_basic_tags(self, description: str) -> List[Dict[str,str]]:
-
         basic_tags = self.generate_basic_tags(description)
         return self.util.remove_duplicates_from_dict_list_based_on_key(basic_tags, "value")
 
-
     def generate_basic_tags(self, description: str) -> List[Dict]:
-
         description_tags: List[Dict] = []
         key_words = self.util.get_key_word_dict(description)
         for word in key_words:
             if self.util.is_token_specific_type(word,"PERSON"):
                 description_tags += self.get_person_tags(word)
-
         return description_tags
