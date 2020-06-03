a705b882f828bbee3cd3afeb96e38ecf28330c01
Austin Marchese <amarchese3@gmail.com>
2020-06-02 21:22:34 -0400

    rotating log files based on time, store 3 days worth of logs

--- src/main/utils/logger_initializer.py
+++ src/main/utils/logger_initializer.py
@@ -3,6 +3,7 @@ Sourced: https://aykutakin.wordpress.com/2013/08/06/logging-to-console-and-file-
 """
 import logging
 import os.path
+from logging.handlers import TimedRotatingFileHandler
 
 def initialize_logger(output_dir):
     logger = logging.getLogger()
@@ -11,27 +12,36 @@ def initialize_logger(output_dir):
     # create console handler and set level to info
     handler = logging.StreamHandler()
     handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    formatter = logging.Formatter("%(asctime)-15s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
     # create error file handler and set level to error
-    handler = logging.FileHandler(os.path.join(output_dir, "logs/error.log"),"w", encoding=None, delay="true")
+    handler = TimedRotatingFileHandler(os.path.join(output_dir, "logs/error.log"),
+                                       when="h",
+                                       interval=24,
+                                       backupCount=3)
     handler.setLevel(logging.ERROR)
-    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    formatter = logging.Formatter("%(asctime)-15s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
     # create debug file handler and set level to debug
-    handler = logging.FileHandler(os.path.join(output_dir, "logs/debug.log"),"w")
+    handler = TimedRotatingFileHandler(os.path.join(output_dir, "logs/debug.log"),
+                                       when="h",
+                                       interval=6,
+                                       backupCount=12)
     handler.setLevel(logging.DEBUG)
-    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    formatter = logging.Formatter("%(asctime)-15s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
-    # create debug file handler and set level to debug
-    handler = logging.FileHandler(os.path.join(output_dir, "logs/info.log"),"w")
+    # create debug file handler and set level to debug, adding rotator
+    handler = TimedRotatingFileHandler(os.path.join(output_dir, "logs/info.log"),
+                                       when="h",
+                                       interval=6,
+                                       backupCount=12)
     handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(levelname)s - %(message)s")
+    formatter = logging.Formatter("%(asctime)-15s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
