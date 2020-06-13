9db861b1a6ad9dfea527ba23b098c25957fb5eb1
Austin Marchese <amarchese3@gmail.com>
2020-06-12 21:47:22 -0400

    Test deployment, altering actuator to see update

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ else:
 @app.route('/actuator')
 def actuator():
     logging.info("actuator")
-    return "success", status.HTTP_200_OK
+    return "success!", status.HTTP_200_OK
 
 @app.route('/')
 def healthcheck():
